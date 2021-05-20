c5f8b407a29fa52a32a58e09309944a0d9e09c7a
Pawandeep singh <sirpawandeep25@gmail.com>
2021-05-20 11:48:47 +0530

    node mailer lost updations

--- routes/index.js
+++ routes/index.js
@@ -21,7 +21,7 @@ var transporter = nodemailer.createTransport({
   
 var mailOptions = {
     from: 'process.env.EMAIL',
-    to: '',
+    to: "req.user.email,process.env.EMAIL",
     subject: 'Welcome to YelpCamp',
     // text:'Welcome to YelpCamp!, You are successfully registered to the YelpCamp. Please go and explore our beautiful website and feel free to drop suggestions / queries at : pawanyelpcamp@gmail.com . Website link: http://pawan-yelp-camp.herokuapp.com/  Regards Pawandeep Singh',
     html: '<h1>Welcome to YelpCamp!</h1><p>You are successfully registered to the YelpCamp.<br> Please go and explore our beautiful website and feel free to drop suggestions / queries at : pawanyelpcamp@gmail.com <br>Website link: http://pawan-yelp-camp.herokuapp.com/ </p><p>Regards <em>Pawandeep Singh</em> </p>'
